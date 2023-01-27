b7b580c6354153e56a1e013d0315c0715285118e
omkarg01 <omkargujja01@gmail.com>
2023-01-27 21:27:20 +0530

    nft minted

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,18 @@
  */
 require("@nomiclabs/hardhat-waffle");
 
-// require("dotenv").config();
-// require("@nomiclabs/hardhat-ethers");
-// const { API_URL, PRIVATE_KEY } = process.env;
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.8.9"
+    solidity: "0.8.9",
+    defaultNetwork: "goerli",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
 };
